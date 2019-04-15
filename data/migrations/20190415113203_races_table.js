b6a901625eb8b7a33cd7a958bb98fb601ead78ae
ills5771 <ilyayelizarov1@gmail.com>
2019-04-15 15:47:50 -0700

    latest version

--- data/migrations/20190415113203_races_table.js
+++ data/migrations/20190415113203_races_table.js
@@ -1,11 +1,38 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("races", tbl => {
-    tbl.increments();
-    tbl.date("date").notNullable();
-    tbl.string("track", 128);
-    tbl.string("city", 128);
-    tbl.string("horses");
-  });
+  return knex.schema
+    .createTable("races", tbl => {
+      tbl.increments();
+      tbl.date("date").notNullable();
+      tbl.string("track", 128);
+      tbl.string("city", 128);
+      tbl.string("horses");
+    })
+    .createTable("results", tbl => {
+      tbl.increments();
+      tbl
+        .integer("race_id")
+        .unsigned()
+        .references("id")
+        .inTable("races");
+
+      tbl
+        .integer("date_id")
+        .unsigned()
+        .references("date")
+        .inTable("races");
+
+      tbl
+        .integer("track_id")
+        .unsigned()
+        .references("track")
+        .inTable("races");
+
+      tbl
+        .integer("city_id")
+        .unsigned()
+        .references("city")
+        .inTable("races");
+    });
 };
 
 exports.down = function(knex) {
