337d5c86ee2923f7367f53b248ba1d53e46c8511
ills5771 <ilyayelizarov1@gmail.com>
2019-04-17 10:35:24 -0700

    adds get enpoint that returns users by id

--- races/racesModel.js
+++ races/racesModel.js
@@ -3,11 +3,11 @@ const db = require("../data/dbConfig.js");
 module.exports = {
   getAll,
   addRace,
-  addUser,
-  getBy,
-  getById,
-  getUsers,
   raceById
+  // addUser,
+  // getBy,
+  // getById,
+  // getUsers,
 };
 
 function getAll() {
@@ -18,27 +18,26 @@ function raceById(id) {
     .where({ id })
     .first();
 }
-
-function getBy(username) {
-  return db("users").where(username);
-}
-
 function addRace(race) {
   return db("races")
     .insert(race)
     .then(ids => ({ id: ids[0] }));
 }
-async function addUser(user) {
-  const [id] = await db("users").insert(user);
+// function getBy(username) {
+//   return db("users").where(username);
+// }
 
-  return getById(id);
-}
+// async function addUser(user) {
+//   const [id] = await db("users").insert(user);
 
-function getUsers() {
-  return db("users");
-}
-function getById(id) {
-  return db("users")
-    .where({ id })
-    .first();
-}
+//   return getById(id);
+// }
+
+// function getUsers() {
+//   return db("users");
+// }
+// function getById(id) {
+//   return db("users")
+//     .where({ id })
+//     .first();
+// }
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const Users = require("../races/racesModel");
+const Users = require("./usersModel.js");
 
 router.get("/", (req, res) => {
   Users.getUsers()
@@ -8,6 +8,25 @@ router.get("/", (req, res) => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await Users.getById(req.params.id);
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The user with that ID does not exist." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "User information could not be retrieved."
+    });
+  }
 });
 
 module.exports = router;
