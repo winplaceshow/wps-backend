3dc57b55fe16f5f59737b20bd5a47bb674c6fb7a
ills5771 <ilyayelizarov1@gmail.com>
2019-04-18 12:20:30 -0700

    Adds token to login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -34,9 +34,11 @@ router.post("/login", (req, res) => {
     Users.getBy({ username })
       .first()
       .then(user => {
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome back ${user.username}!`,
-          id: user.id
+          id: user.id,
+          token
         });
         // if (user && bcrypt.compareSync(password, user.password)) {
         //   const token = generateToken(user);
@@ -55,14 +57,14 @@ router.post("/login", (req, res) => {
       });
   }
 });
-// function generateToken(user) {
-//   const payload = {
-//     username: user.username
-//   };
-//   const options = {
-//     expiresIn: "3d"
-//   };
-//   return jwt.sign(payload, secret, options);
-// }
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "3d"
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
